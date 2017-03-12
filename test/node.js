68ad6a4372cd946e0e09af3dfd2a07d2ba7931b0
Artyom Egorov <mail@egoroof.ru>
2017-03-12 16:47:15 +0700

    Simplify nodejs crypto testing

--- test/node.js
+++ test/node.js
@@ -13,63 +13,42 @@ tests.forEach((testPack) => {
 });
 
 describe('nodejs crypto', () => {
+    it('ECB', () => {
+        const key = 'super key';
+        const text = 'Encoded string';
+        const encodedText = new Uint8Array([192, 230, 228, 19, 197, 108, 172, 217, 61, 169, 178, 167, 62, 197, 94, 45]);
 
-    describe('mode ECB, padding PKCS5', () => {
-        it('should correctly encode english text', () => {
-            const key = 'super key';
-            const text = 'Encoded string';
+        const bf = new Blowfish(key, Blowfish.MODE.ECB, Blowfish.PADDING.PKCS5);
+        const encoded = bf.encode(text);
+        const decoded = bf.decode(encodedText, Blowfish.TYPE.UINT8_ARRAY);
 
-            const bf = new Blowfish(key, Blowfish.MODE.ECB, Blowfish.PADDING.PKCS5);
-            const encoded = bf.encode(text);
+        const nodeBfCipher = crypto.createCipheriv('bf-ecb', key, '');
+        const nodeEncoded = Buffer.concat([nodeBfCipher.update(text, 'utf8'), nodeBfCipher.final()]);
 
-            const nodeBf = crypto.createCipheriv('bf-ecb', key, '');
-            const nodeEncoded = Buffer.concat([nodeBf.update(text, 'utf8'), nodeBf.final()]);
+        const nodeBfDecipher = crypto.createDecipheriv('bf-ecb', key, '');
+        const nodeDecoded = Buffer.concat([nodeBfDecipher.update(Buffer.from(encodedText)), nodeBfDecipher.final()]);
 
-            expect(encoded).to.eql(new Uint8Array(nodeEncoded));
-        });
-        it('should correctly decode english text', () => {
-            const key = 'super key';
-            const encodedText = new Uint8Array([192, 230, 228, 19, 197, 108, 172, 217, 61, 169, 178, 167, 62, 197, 94, 45]);
-
-            const bf = new Blowfish(key, Blowfish.MODE.ECB, Blowfish.PADDING.PKCS5);
-            const decoded = bf.decode(encodedText, Blowfish.TYPE.UINT8_ARRAY);
-
-            const nodeBf = crypto.createDecipheriv('bf-ecb', key, '');
-            const nodeDecoded = Buffer.concat([nodeBf.update(Buffer.from(encodedText)), nodeBf.final()]);
-
-            expect(decoded).to.eql(new Uint8Array(nodeDecoded));
-        });
+        expect(encoded).to.eql(new Uint8Array(nodeEncoded));
+        expect(decoded).to.eql(new Uint8Array(nodeDecoded));
     });
+    it('CBC', () => {
+        const key = 'super key';
+        const iv = 'abcdefhi';
+        const text = 'Encoded string';
+        const encodedText = new Uint8Array([229, 58, 64, 10, 60, 227, 46, 82, 62, 73, 108, 227, 74, 220, 29, 0]);
 
-    describe('mode CBC, padding PKCS5', () => {
-        it('should correctly encode english text', () => {
-            const key = 'super key';
-            const iv = 'abcdefhi';
-            const text = 'Encoded string';
+        const bf = new Blowfish(key, Blowfish.MODE.CBC, Blowfish.PADDING.PKCS5);
+        bf.setIv(iv);
+        const encoded = bf.encode(text);
+        const decoded = bf.decode(encodedText, Blowfish.TYPE.UINT8_ARRAY);
 
-            const bf = new Blowfish(key, Blowfish.MODE.CBC, Blowfish.PADDING.PKCS5);
-            bf.setIv(iv);
-            const encoded = bf.encode(text);
+        const nodeBfCipher = crypto.createCipheriv('bf-cbc', key, iv);
+        const nodeEncoded = Buffer.concat([nodeBfCipher.update(text, 'utf8'), nodeBfCipher.final()]);
 
-            const nodeBf = crypto.createCipheriv('bf-cbc', key, iv);
-            const nodeEncoded = Buffer.concat([nodeBf.update(text, 'utf8'), nodeBf.final()]);
+        const nodeBfDecipher = crypto.createDecipheriv('bf-cbc', key, iv);
+        const nodeDecoded = Buffer.concat([nodeBfDecipher.update(Buffer.from(encodedText)), nodeBfDecipher.final()]);
 
-            expect(encoded).to.eql(new Uint8Array(nodeEncoded));
-        });
-        it('should correctly decode english text', () => {
-            const key = 'super key';
-            const iv = 'abcdefhi';
-            const encodedText = new Uint8Array([229, 58, 64, 10, 60, 227, 46, 82, 62, 73, 108, 227, 74, 220, 29, 0]);
-
-            const bf = new Blowfish(key, Blowfish.MODE.CBC, Blowfish.PADDING.PKCS5);
-            bf.setIv(iv);
-            const decoded = bf.decode(encodedText, Blowfish.TYPE.UINT8_ARRAY);
-
-            const nodeBf = crypto.createDecipheriv('bf-cbc', key, iv);
-            const nodeDecoded = Buffer.concat([nodeBf.update(Buffer.from(encodedText)), nodeBf.final()]);
-
-            expect(decoded).to.eql(new Uint8Array(nodeDecoded));
-        });
+        expect(encoded).to.eql(new Uint8Array(nodeEncoded));
+        expect(decoded).to.eql(new Uint8Array(nodeDecoded));
     });
-
 });
